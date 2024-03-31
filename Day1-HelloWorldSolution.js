dd73fb24a0afcf69d828ff30fc60d014a69462d1
Mukul <msoni7718@gmail.com>
2024-04-01 00:03:05 +0530

    Day 1

---
+++ Day1-HelloWorldSolution.js
@@ -0,0 +1,15 @@
+/*
+var createHelloWorld = function() {
+    
+    return function(...args) {
+	console.log('Hello World');
+    }
+};
+console.log(createHelloWorld);
+*/
+
+function createHelloWorld(){
+	console.log("Hello World");
+}
+
+createHelloWorld();
